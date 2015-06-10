e888a230d644e85bccc81392da9cddec1171d534
Tian Davis <tiandavis@gmail.com>
2015-06-10 18:25:51 -0500

    Added console output for tests.

--- src/main/com/digium/respoke/Respoke.java
+++ src/main/com/digium/respoke/Respoke.java
@@ -3,7 +3,15 @@ package com.digium.respoke;
 import org.apache.commons.lang3.StringUtils;
 
 public class Respoke {
+	private String tokenId;
+	
 	public Respoke() {
 		System.out.println("Hello World!");
+		
+		tokenId = new String();
+	}
+	
+	public String getTokenId() {
+		return tokenId;
 	}
 }
