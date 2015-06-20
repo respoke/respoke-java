5a4aae1785f2d01ec30ba8c95cd1d5f9db67905f
Tian Davis <tiandavis@gmail.com>
2015-06-20 03:45:00 -0400

    Jar all dependencies using Gradle. Start making basic HTTP test calls.

--- src/main/com/digium/respoke/Respoke.java
+++ src/main/com/digium/respoke/Respoke.java
@@ -7,6 +7,23 @@ import org.apache.commons.lang3.StringUtils;
 import lombok.Getter;
 import lombok.Setter;
 
+/*import org.json.*;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.*;
+import com.mashape.unirest.http.exceptions.UnirestException;*/
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.async.Callback;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import com.mashape.unirest.http.options.Options;
+import com.mashape.unirest.request.GetRequest;
+
 public class Respoke {
 	@Getter @Setter
 	private String appId;
@@ -43,6 +60,42 @@ public class Respoke {
 	}
 	
 	public String getTokenId() {
-		return tokenId;
+		String token = "";
+			
+		try {
+			HttpResponse<JsonNode> tokenRespoke = Unirest.post("http://httpbin.org/post")
+				.header("accept", "application/json")
+				.queryString("apiKey", "123")
+				.field("parameter", "value")
+				.field("foo", "bar")
+				.asJson();
+				
+			token = tokenRespoke.getBody().toString();
+			
+			System.out.println(token);
+		} 
+		catch(Exception e) {
+			System.out.println(e.toString());
+		}
+		
+		return token;
+		
+		/*return Unirest.post("http://httpbin.org/post")
+			  .queryString("name", "Mark")
+			  .field("last", "Polo")
+			  .asString();*/
+		
+		/*HttpResponse<JsonNode> jsonResponse = Unirest.post("http://httpbin.org/post")
+			  .header("accept", "application/json")
+			  .field("param1", "value1")
+			  .field("param2","bye")
+			  .asJson();
+
+		return jsonResponse.getBody().toString();*/
+		
+		/*return Unirest.post("http://httpbin.org/post")
+					  .queryString("name", "Mark")
+					  .field("last", "Polo")
+					  .asJson().getBody().toString();*/
 	}
 }
--- tests/main/com/digium/respoke/RespokeTest.java
+++ tests/main/com/digium/respoke/RespokeTest.java
@@ -27,7 +27,6 @@ public class RespokeTest {
 							 "371F82D1-E4CE-4BB0-B2BB-79EA3497FC4F",
 							 "spock@enterprise.com");
 		
-		assertEquals((client instanceof Respoke), true);
 		assertEquals(client.getAppId(), "c10a2075-3f3d-466f-82f9-d2285e64c5d4");
 		assertEquals(client.getAppSecret(), "eb327e57-e766-49de-b801-ef612a70509e");
 		assertEquals(client.getRoleId(), "371F82D1-E4CE-4BB0-B2BB-79EA3497FC4F");
@@ -43,10 +42,36 @@ public class RespokeTest {
 			put("endpointId", "spock@enterprise.com");
 		}});
 		
-		assertEquals((client instanceof Respoke), true);
 		assertEquals(client.getAppId(), "c10a2075-3f3d-466f-82f9-d2285e64c5d4");
 		assertEquals(client.getAppSecret(), "eb327e57-e766-49de-b801-ef612a70509e");
 		assertEquals(client.getRoleId(), "371F82D1-E4CE-4BB0-B2BB-79EA3497FC4F");
 		assertEquals(client.getEndpointId(), "spock@enterprise.com");
+	}
+	
+	@Test
+	public void ValidateAccessors() {
+		client = new Respoke();
+		
+		client.setAppId("c10a2075-3f3d-466f-82f9-d2285e64c5d4");
+		client.setAppSecret("eb327e57-e766-49de-b801-ef612a70509e");
+		client.setRoleId("371F82D1-E4CE-4BB0-B2BB-79EA3497FC4F");
+		client.setEndpointId("spock@enterprise.com");
+			
+		assertEquals(client.getAppId(), "c10a2075-3f3d-466f-82f9-d2285e64c5d4");
+		assertEquals(client.getAppSecret(), "eb327e57-e766-49de-b801-ef612a70509e");
+		assertEquals(client.getRoleId(), "371F82D1-E4CE-4BB0-B2BB-79EA3497FC4F");
+		assertEquals(client.getEndpointId(), "spock@enterprise.com");
+	}
+	
+	@Test
+	public void GetTokenId() {
+		client = new Respoke(new HashMap<String, String>() {{
+			put("appId", "c10a2075-3f3d-466f-82f9-d2285e64c5d4");
+			put("appSecret", "eb327e57-e766-49de-b801-ef612a70509e");
+			put("roleId", "371F82D1-E4CE-4BB0-B2BB-79EA3497FC4F");
+			put("endpointId", "spock@enterprise.com");
+		}});
+		
+		assertEquals(client.getTokenId(), "c10a2075-3f3d-466f-82f9-d2285e64c5d4");
 	}
 }
