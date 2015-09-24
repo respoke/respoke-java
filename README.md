1052262f314e371ddd2cb722285d099b9e685d2a
Chad McElligott <cmcelligott@digium.com>
2015-09-24 11:53:47 -0500

    Cleanup codebase
    This patch introduces gradlew scripts, and moves to spaces instead of
    tabs and standardizes the codebase for that using .editorconfig.

--- README.md
+++ README.md
@@ -1,62 +1,75 @@
-# Respoke Java Library [![Build Status](https://travis-ci.org/respoke/respoke-java.svg)](https://travis-ci.org/respoke/respoke-java)
+# Respoke Java Library
+[![Build Status](https://travis-ci.org/respoke/respoke-java.svg)](https://travis-ci.org/respoke/respoke-java)
 
-Respoke Java is the officially supported Java library for [Respoke](https://respoke.io). 
+Respoke Java is the officially supported Java library for [Respoke](https://respoke.io).
 
-With Respoke, you can add live voice, video, text and data features to your website or mobile app. Check out our [Guides](https://docs.respoke.io/server/java/getting-started.html) to get started using Respoke and Respoke Java now.
+With Respoke, you can add live voice, video, text and data features to your website or mobile app. Check out our
+[Guides](https://docs.respoke.io/server/java/getting-started.html) to get started using Respoke and Respoke Java now.
 
 Please validate you have Java 7 or greater installed.
 
 ## Installation
 
-Install [Respoke's Java Library](http://search.maven.org/#search%7Cga%7C1%7Crespoke) to your project using [Maven](http://search.maven.org/).
+Install [Respoke's Java Library](http://search.maven.org/#search%7Cga%7C1%7Crespoke) to your project using
+[Maven](http://search.maven.org/).
 
-    <dependency>
-        <groupId>com.digium.respoke</groupId>
-        <artifactId>respoke-java</artifactId>
-        <version>1.0.0</version>
-    </dependency>
+```
+<dependency>
+    <groupId>com.digium.respoke</groupId>
+    <artifactId>respoke-java</artifactId>
+    <version>1.0.0</version>
+</dependency>
+```
 
 Install Respoke's Java Library to your project using [Gradle](https://gradle.org/).
 
-    dependencies {	
-    	compile "com.digium.respoke:respoke-java:1.0.0"
-    }
-    
+```
+dependencies {
+    compile "com.digium.respoke:respoke-java:1.0.0"
+}
+```
+
 ## Running the library
 
 To use the library, sign up for a FREE [Respoke account](https://portal.respoke.io/#/signup).
 
-    import com.digium.respoke.*;
-    
-	Respoke client = new Respoke(new HashMap<String, String>() {{
-		put("appId", "APP_ID");
-		put("appSecret", "APP_SECRET");
-		put("roleId", "ROLE_ID");
-		put("endpointId", "USER_NAME");
-	}});
-    
-    String tokenId = client.getTokenId();
-    
+```java
+import com.digium.respoke.*;
+
+Respoke client = new Respoke(new HashMap<String, String>() {{
+    put("appId", "APP_ID");
+    put("appSecret","APP_SECRET");
+    put("roleId", "ROLE_ID");
+    put("endpointId", "USER_NAME");
+}});
+
+String tokenId = client.getTokenId();
+```
+
 Return this `tokenId` to your front-end and pass it to the `token` property when connecting to Respoke.
-    
-## Building from source && Running Tests
+
+## Building from source & Running Tests
 
 To build the project from source.
 
-    gradle build
-    
+```sh
+./gradlew build
+```
+
 To run the tests from source.
-    
-    gradle check
-    
+
+```sh
+./gradlew check
+```
+
 To upload to the maven central respository.
 
-    gradle -b maven.gradle
-    gradle -b maven.gradle uploadArchives
-    
-Then close and release the library.
+```sh
+./gradlew -b maven.gradle gradle -b maven.gradle uploadArchives
+```
 
-http://central.sonatype.org/pages/releasing-the-deployment.html
+Then close and [release the library](http://central.sonatype.org/pages/releasing-the-deployment.html).
+
 
 ## Contributing
 
--- src/main/com/digium/respoke/Respoke.java
+++ src/main/com/digium/respoke/Respoke.java
@@ -30,94 +30,91 @@ import com.mashape.unirest.request.GetRequest;
  * The top-level wrapper around the Respoke REST API.
  */
 public class Respoke {
-	@Getter @Setter
-	private String appId;
-	
-	@Getter @Setter
-	private String appSecret;
-	
-	@Getter @Setter
-	private String roleId;
-	
-	@Getter @Setter
-	private String endpointId;
-	
-	@Setter
-	private String tokenId;
-	
-	
-	/**
-	 *  Respoke Constructor
-	 *
-	 */
-	public Respoke() {
-		
-	}
-	
-	/**
-	 *  Respoke Constructor
-	 *
-	 *  @param appId The App Id generated when you create a Respoke app.
-	 *  @param appSecret The App Secret generated when you create a Respoke app.
-	 *  @param roleId The Role Id you create so your App handle authorization.
-	 *  @param endpointId The username for the user being authenticated.
-	 */
-	public Respoke(String appId, String appSecret, String roleId, String endpointId) {
-		this.appId = appId;
-		this.appSecret = appSecret;
-		this.roleId = roleId;
-		this.endpointId = endpointId;
-	}
-	
-	/**
-	 *  Respoke Constructor
-	 *
-	 *  @param properties A HashMap containing values for appId, appSecret, roleId and endpointId.
-	 *
-	 * <pre>
-	 * {@code
-	 *   Respoke client = new Respoke(new HashMap<String, String>() {{  
-	 *      put("appId", "c10a2075-3f3d-466f-82f9-d2285e64c5d4");
-	 *      put("appSecret", "eb327e57-e766-49de-b801-ef612a70509e");
-	 *      put("roleId", "371F82D1-E4CE-4BB0-B2BB-79EA3497FC4F");
-	 *      put("endpointId", "spock@enterprise.com");
-	 *   }});
-	 * }
-	 * </pre>
-	 */
-	public Respoke(HashMap<String, String> properties) {
-		appId = properties.get("appId");
-		appSecret = properties.get("appSecret");
-		roleId = properties.get("roleId");
-		endpointId = properties.get("endpointId");
-	}
-	
-	/**
-	 *  Create a temporary tokenId to use when connecting to Respoke.
-	 *
-	 *  @return tokenId The temporary tokenId returned from Repoke.
-	 */
-	public String getTokenId() {
-		String tokenId = "";
-			
-		try {
-			HttpResponse<JsonNode> tokenRespoke = Unirest.post("https://api.respoke.io/v1/tokens")
-				.header("Content-type", "application/json")
-				.header("App-Secret", appSecret)
-				.body(new JSONObject()
-					.put("appId", appId)
-					.put("endpointId", endpointId)
-					.put("roleId", roleId)
-					.put("ttl", "3600")
-					.toString())
-				.asJson();
-				
-			tokenId = tokenRespoke.getBody().getObject().getString("tokenId");
-		} 
-		catch(UnirestException e) {
+    @Getter @Setter
+    private String appId;
 
-		}
-		
-		return tokenId;
-	}
+    @Getter @Setter
+    private String appSecret;
+
+    @Getter @Setter
+    private String roleId;
+
+    @Getter @Setter
+    private String endpointId;
+
+    @Setter
+    private String tokenId;
+
+    /**
+     *  Respoke Constructor
+     */
+    public Respoke() {
+
+    }
+
+    /**
+     *  Respoke Constructor
+     *
+     *  @param appId The App Id generated when you create a Respoke app.
+     *  @param appSecret The App Secret generated when you create a Respoke app.
+     *  @param roleId The Role Id you create so your App handle authorization.
+     *  @param endpointId The username for the user being authenticated.
+     */
+    public Respoke(String appId, String appSecret, String roleId, String endpointId) {
+        this.appId = appId;
+        this.appSecret = appSecret;
+        this.roleId = roleId;
+        this.endpointId = endpointId;
+    }
+
+    /**
+     *  Respoke Constructor
+     *
+     *  @param properties A HashMap containing values for appId, appSecret, roleId and endpointId.
+     *
+     * <pre>
+     * {@code
+     *   Respoke client = new Respoke(new HashMap<String, String>() {{
+     *      put("appId", "c10a2075-3f3d-466f-82f9-d2285e64c5d4");
+     *      put("appSecret", "eb327e57-e766-49de-b801-ef612a70509e");
+     *      put("roleId", "371F82D1-E4CE-4BB0-B2BB-79EA3497FC4F");
+     *      put("endpointId", "spock@enterprise.com");
+     *   }});
+     * }
+     * </pre>
+     */
+    public Respoke(HashMap<String, String> properties) {
+        appId = properties.get("appId");
+        appSecret = properties.get("appSecret");
+        roleId = properties.get("roleId");
+        endpointId = properties.get("endpointId");
+    }
+
+    /**
+     *  Create a temporary tokenId to use when connecting to Respoke.
+     *
+     *  @return tokenId The temporary tokenId returned from Repoke.
+     */
+    public String getTokenId() {
+        String tokenId = "";
+
+        try {
+            HttpResponse<JsonNode> tokenRespoke = Unirest.post("https://api.respoke.io/v1/tokens")
+                .header("Content-type", "application/json")
+                .header("App-Secret", appSecret)
+                .body(new JSONObject()
+                        .put("appId", appId)
+                        .put("endpointId", endpointId)
+                        .put("roleId", roleId)
+                        .put("ttl", "3600")
+                        .toString())
+                .asJson();
+
+            tokenId = tokenRespoke.getBody().getObject().getString("tokenId");
+        } catch(UnirestException e) {
+
+        }
+
+        return tokenId;
+    }
 }
